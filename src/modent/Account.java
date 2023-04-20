112bb24841ab8fde392b52e8e84977239243755c
nguyennhuhung281197 <125869328+nguyennhuhung281197@users.noreply.github.com>
2023-04-20 10:55:46 +0700

    Class Account

---
+++ src/modent/Account.java
@@ -0,0 +1,47 @@
+package modent;
+
+public class Account  {
+    private int accountNumber;
+    private String nameAccount;
+    private double accountAmount;
+
+    public Account() {
+    }
+
+    public Account(int accountNumber, String nameAccount, double accountAmount) {
+        this.accountNumber = accountNumber;
+        this.nameAccount = nameAccount;
+        this.accountAmount = accountAmount;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getNameAccount() {
+        return nameAccount;
+    }
+
+    public void setNameAccount(String nameAccount) {
+        this.nameAccount = nameAccount;
+    }
+
+    public double getAccountAmount() {
+        return accountAmount;
+    }
+
+    public void setAccountAmount(double accountAmount) {
+        this.accountAmount = accountAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "" + '\t'+'\t'+'\t' + accountNumber + '\t'+'\t'+'\t' + nameAccount + '\t'+'\t'+'\t' + accountAmount + "$";
+    }
+
+
+}
