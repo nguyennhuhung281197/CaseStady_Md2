c3673514a7c29df0fc82761c78a0b47d6cbcf8c3
nguyennhuhung281197 <125869328+nguyennhuhung281197@users.noreply.github.com>
2023-04-20 10:58:15 +0700

    Abstract Method

---
+++ src/base/AbstractMethod.java
@@ -0,0 +1,9 @@
+package base;
+
+public abstract class AbstractMethod {
+    public abstract void display();
+    public abstract void addAccount();
+    public abstract boolean removeAccount();
+    public abstract void updateAccount();
+
+}
