010394806b8073026e54b9fc961e24229bdb7607
nguyennhuhung281197 <125869328+nguyennhuhung281197@users.noreply.github.com>
2023-04-23 13:43:13 +0700

    Trien khai  past 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,75 +1,44 @@
 package main;
 
 import controller.ManageAccount;
-import fileio.ReadFile;
-import fileio.WriteFile;
-import modent.Account;
+import controller.ManageUser;
+import model.User;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        ManageAccount manageAccount = new ManageAccount();
-
-        ManageAccount.Used();
-
-        boolean isRun = true;
-        while (isRun) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("""
-                     =======================================
-                     | 1.Add User Information              |
-                     | 2.Display Information               |
-                     | 3.Edit Information                  |
-                     | 4.Delete Information                |
-                     | 5.Search Name Information           |
-                     | 6.Sort By Account Number            |
-                     | 7.Deposit Money Into Your Account   |
-                     | 8.Withdraw Money                    |
-                     | 9.Transfer Money                    |
-                     | 10.Ghi file                         |
-                     =======================================
-                    """);
-            System.out.println("PLEASE ENTER : ");
+        ManageUser manageUser = new ManageUser();
+        Scanner sc = new Scanner(System.in);
+        boolean check = true;
+        while (check) {
+            System.out.println("1. Register");
+            System.out.println("2.Log In");
+            System.out.println("3. Exit");
+            System.out.println("Your choice : ");
             int choice = sc.nextInt();
             sc.nextLine();
-
             switch (choice) {
                 case 1:
-                    manageAccount.addAccount();
+                    manageUser.register();
                     break;
                 case 2:
-                    manageAccount.display();
-
+                    manageUser.login();
                     break;
                 case 3:
-                    manageAccount.updateAccount();
+                    System.exit(0);
+                    check = false;
                     break;
-                case 4:
-                    manageAccount.removeAccount();
-                    break;
-                case 5:
-                    manageAccount.searchAccount();
-                    break;
-                case 6:
-                    manageAccount.sortAccount();
 
+                default:
+                    System.err.println("""
+                            +--------------------+
+                            | Invalid selection! |
+                            +--------------------+""");
                     break;
-                case 7:
-                    manageAccount.loadMoney();
-                    break;
-                case 8:
-                    manageAccount.withdrawMoney();
-                    break;
-                case 9:
-                    manageAccount.transferMoney();
-                    break;
-                case 0:
-                    isRun = false;
-                    break;
+
+
             }
         }
 
